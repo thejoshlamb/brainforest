61c0aa76779a7cc97146c117069e7d4dd011afdb
thejoshlamb <joshlamb@gmail.com>
2013-11-08 15:55:45 -0500

    added delete function

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
 
 	validates :description, :name, :presence => true
-	validates :price_in_cents, :numericality => {:only_integer => true}
-	validates :price_in_cents, :numericality => {:greater_than => 0}
+	validates :price_in_cents, :numericality => {:only_integer => true, :greater_than => 0}
+	
 end
