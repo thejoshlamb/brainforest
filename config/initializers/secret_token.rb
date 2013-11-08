fcf97304b859af15a67e3ab581247e26b39ed162
thejoshlamb <joshlamb@gmail.com>
2013-11-08 14:00:17 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Brainforest::Application.config.secret_key_base = '39002731a79d6db573c21b3c90d907ccc1281d8ed076e34a5e167236091e92f7ccd80209e1e79147f2ebaa2117ae793e4b3137bbb5c000ce9a33f4dc2944b120'
